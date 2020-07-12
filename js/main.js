5df1f864d6fa988fd0a133a76c465020f468c5e0
Ritik Batra <ritikbatra@berkeley.edu>
2020-07-12 12:52:23 -0700

    Only remaining parts are services/resume

--- js/main.js
+++ js/main.js
@@ -185,12 +185,11 @@
 				$('.number').each(function(){
 					var $this = $(this),
 						num = $this.data('number');
-						console.log(num);
 					$this.animateNumber(
 					  {
 					    number: num,
 					    numberStep: comma_separator_number_step
-					  }, 7000
+					  }, 4000
 					);
 				});
 				
